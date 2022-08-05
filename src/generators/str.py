a7e091d28c40197575734d649e96dccd3136fbdc
aleksandr.ryabchenko <aleksandr.ryabchenko@mysky.com>
2022-08-06 00:50:23 +0300

    Make version on generators

---
+++ src/generators/str.py
@@ -0,0 +1,3 @@
+def randstr_generator():
+    while True:
+        yield 'todo_rand_str'
--- src/main.py
+++ src/main.py
@@ -1,9 +1,11 @@
 from random import randint
 
 from database import User
+from generators.int import randint_generator
+from generators.str import randstr_generator
 
 
-def generate_random_data(input_class, count: int):
+def generate_random_data_v0(input_class, count: int):
     for _ in range(count):
         raw_data = dict()
         for key, key_class in input_class.__annotations__.items():
@@ -15,6 +17,22 @@ def generate_random_data(input_class, count: int):
         yield res
 
 
+def generate_random_data_v1(input_class, count: int):
+    key_to_generator = {}
+    for key, key_class in input_class.__annotations__.items():
+        if key_class == int:
+            key_to_generator[key] = randint_generator()
+        elif key_class == str:
+            key_to_generator[key] = randstr_generator()
+
+    for _ in range(count):
+        yield input_class(
+            **{
+                key: next(gen) for key, gen in key_to_generator.items()
+            }
+        )
+
+
 if __name__ == '__main__':
-    for rec in generate_random_data(User, count=3):
+    for rec in generate_random_data_v1(User, count=3):
         print(rec)

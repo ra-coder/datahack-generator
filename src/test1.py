21e2ed7b64dde48d5d6929aa61fab2261bc1ed31
aleksandr.ryabchenko <aleksandr.ryabchenko@mysky.com>
2022-08-06 16:41:38 +0300

    Fix test

--- src/test1.py
+++ src/test1.py
@@ -2,10 +2,9 @@ from pyspark.sql import SparkSession
 
 spark = SparkSession.builder.appName("data hack").config(
     "spark.driver.host", "localhost"
-    # ).config(
-    #     "spark.driver.port", "7077"
 ).getOrCreate()
 
-df = spark.read.parquet("output/user.parquet")
-df.show()
+for name in ['user', 'schedule', 'company', 'category']:
+    df = spark.read.parquet(f"output/{name}.parquet")
+    df.show()
 spark.stop()

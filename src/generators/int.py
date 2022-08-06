bebb1ed433e6c2b65579d1fe4347101b532cbace
aleksandr.ryabchenko <aleksandr.ryabchenko@mysky.com>
2022-08-06 11:04:55 +0300

    Refactor use of faker

--- src/generators/int.py
+++ src/generators/int.py
@@ -1,10 +1,14 @@
 from random import randint
 from faker import Faker
 
-fake = Faker()
-Faker.seed(0)
-
 
 def rand_int_generator(from_value=0, to_value=100500):
+    while True:
+        yield randint(from_value, to_value)
+
+
+def rand_int_generator_faker(from_value=0, to_value=100500, seed=1234):
+    fake = Faker()
+    Faker.seed(seed)
     while True:
         yield fake.bothify(text=f'{randint(from_value, to_value)} #####')
--- src/generators/str.py
+++ src/generators/str.py
@@ -1,9 +1,7 @@
 from faker import Faker
 
 
-fake = Faker()
-
-
 def rand_str_generator():
+    fake = Faker()
     while True:
         yield fake.pystr(min_chars=1, max_chars=20)
